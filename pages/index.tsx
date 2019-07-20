576de865d91717d4b5c5233a66c10c7bca691093
Junyoung Choi <fluke8259@gmail.com>
2019-07-21 01:47:10 +0900

    Update index page

---
+++ pages/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import DefaultTemplate from '../templates/DefaultTemplate'
+import {
+  fetchPostsIndexResponseBody,
+  PostsIndexResponseBody
+} from '../api/posts'
+import PostCard from '../molecules/PostCard'
+
+interface HomePageProps {
+  data: PostsIndexResponseBody
+}
+
+const HomePage = ({ data }: HomePageProps) => (
+  <DefaultTemplate>
+    <header>
+      <h1>Rokt33r's blog</h1>
+      <p>Mainly talking about Typescript, Node.js, React.js and Markdown.</p>
+    </header>
+    <section>
+      <h2>Recent posts</h2>
+      <div>
+        {data.posts.map(post => (
+          <PostCard key={post.name} post={post} />
+        ))}
+      </div>
+    </section>
+  </DefaultTemplate>
+)
+
+HomePage.getInitialProps = async () => {
+  const data = await fetchPostsIndexResponseBody()
+  return { data }
+}
+
+export default HomePage
