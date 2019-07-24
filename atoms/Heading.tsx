bd6772fbc9966dd8339395cb641c8b3614b86c93
Junyoung Choi <fluke8259@gmail.com>
2019-07-24 09:42:32 +0900

    Refactor Heading

--- atoms/Heading.tsx
+++ atoms/Heading.tsx
@@ -2,15 +2,17 @@ import Text, { TextProps } from './Text'
 import styled from 'styled-components'
 
 interface HeadingProps extends TextProps {
+  depth?: number
   as?: keyof JSX.IntrinsicElements | React.ComponentType<any>
 }
 
-const Heading = styled(Text)<HeadingProps>({})
+const Heading = styled(Text).attrs(({ depth = 2, fontSize }: HeadingProps) => ({
+  as: `h${depth}`,
+  fontSize: fontSize != null ? fontSize : 6 - depth
+}))<HeadingProps>({})
 
 Heading.defaultProps = {
-  as: 'h2',
   m: 0,
-  fontSize: 4,
   fontWeight: 'bold'
 }
 
