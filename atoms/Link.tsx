9fc0b1848d3d88faeba1593f2d38b8edaaec4abb
Junyoung Choi <fluke8259@gmail.com>
2019-07-28 12:33:22 +0900

    Refactor Link

--- atoms/Link.tsx
+++ atoms/Link.tsx
@@ -1,13 +1,33 @@
 import NextLink from 'next/link'
-import BaseLink, { BaseLinkProps } from './BaseLink'
+import styled from 'styled-components'
+import Text, { TextProps } from './Text'
 
-interface LinkProps extends BaseLinkProps {
+interface LinkElementProps extends TextProps {
+  as: 'a'
+  href?: string
+  target?: '_blank'
+}
+
+const LinkElement = styled(Text)<LinkElementProps>({
+  color: 'inherit',
+  '&:hover': {
+    color: 'blue'
+  }
+})
+
+interface LinkProps {
   href: string
+  newTab?: boolean
   children: React.ReactNode
 }
 
-export default ({ href, children }: LinkProps) => (
-  <NextLink href={href} passHref={true}>
-    <BaseLink>{children}</BaseLink>
-  </NextLink>
-)
+export default ({ href, children, newTab }: LinkProps) =>
+  newTab ? (
+    <LinkElement href={href} as='a' target='_blank'>
+      {children}
+    </LinkElement>
+  ) : (
+    <NextLink href={href} passHref={true}>
+      <LinkElement as='a'>{children}</LinkElement>
+    </NextLink>
+  )
