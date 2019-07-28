bbeee5f55fc0eede8b23f1b6efb37121901cb2e6
Junyoung Choi <fluke8259@gmail.com>
2019-07-28 15:01:46 +0900

    Set global style and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,49 @@
 import App, { Container, AppContext, AppProps } from 'next/app'
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import { GlobalDataContext } from '../lib/global'
 import { fetchGlobalData, GlobalResponseBody } from '../api/global'
 
-const theme = {
-  '*': { boxSizing: 'border-box' },
-  body: {
-    margin: 0
-  }
+const colors = {
+  navy: '#001F3F',
+  blue: '#0074D9',
+  aqua: '#7FDBFF',
+  teal: '#39CCCC',
+  olive: '#3D9970',
+  green: '#2ECC40',
+  lime: '#01FF70',
+  yellow: '#FFDC00',
+  orange: '#FF851B',
+  red: '#FF4136',
+  fuchsia: '#F012BE',
+  purple: '#B10DC9',
+  maroon: '#85144B',
+  white: '#FFFFFF',
+  silver: '#DDDDDD',
+  gray: '#AAAAAA',
+  black: '#111111'
 }
+
+const theme = {
+  breakpoints: ['40em', '52em', '64em', '80em'],
+  badges: {
+    primary: {
+      color: '#FFF',
+      backgroundColor: colors.blue
+    }
+  },
+  colors
+}
+
+const GlobalStyle = createGlobalStyle<{ theme: typeof theme }>`
+  * { box-sizing: border-box; }
+  body {
+    margin: 0;
+    color: ${colors.black};
+    font-family: Ubuntu, sans-serif;
+    line-height: 1.625;
+  }
+`
 
 interface MyAppProps {
   globalData: GlobalResponseBody
@@ -32,6 +66,7 @@ export default class MyApp extends App<MyAppProps> {
       <Container>
         <ThemeProvider theme={theme}>
           <GlobalDataContext.Provider value={globalData}>
+            <GlobalStyle />
             <Component {...pageProps} />
           </GlobalDataContext.Provider>
         </ThemeProvider>
