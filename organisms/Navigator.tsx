866f3b6a57ba727fbaac59ec4f738ec4b5e07df7
Junyoung Choi <fluke8259@gmail.com>
2019-07-31 00:50:46 +0900

    Improve Navigator style

--- organisms/Navigator.tsx
+++ organisms/Navigator.tsx
@@ -1,22 +1,89 @@
 import React from 'react'
-import Box from '../atoms/Box'
+import Box, { BoxProps } from '../atoms/Box'
 import Link from '../atoms/Link'
-import Heading from '../atoms/Heading'
 import { useGlobalData } from '../lib/global'
+import styled from 'styled-components'
+import { useRouter } from 'next/router'
+
+const NavContainer = styled(Box)({
+  height: '100vh',
+  top: 0,
+  position: 'sticky'
+})
+
+const NavLinkList = styled(Box).attrs({
+  as: 'ul'
+})`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`
+
+const Avartar = styled(Box).attrs({
+  as: 'img',
+  src: 'https://avatars3.githubusercontent.com/u/5865853?s=40&v=4',
+  width: 20,
+  height: 20,
+  mr: 2
+})``
+
+interface NavLinkListItemProps extends BoxProps {
+  href: string
+  children: React.ReactNode
+  newTab?: boolean
+  active?: boolean
+}
+
+const NavLinkListItem = ({
+  href,
+  children,
+  newTab,
+  ...props
+}: NavLinkListItemProps) => {
+  const { asPath } = useRouter()
+  const active = asPath === href
+
+  return (
+    <Box as='li' {...(props as any)}>
+      <Link
+        {...{ href, children, newTab }}
+        color={active ? 'inherit' : '#777'}
+        textDecoration='none'
+        textStyle='monospace'
+        pl={3}
+        py={1}
+        backgroundColor={active ? 'rgba(0, 0, 0, 0.1)' : undefined}
+        css={`
+          display: flex;
+          align-items: center;
+          transition: background-color ease-in-out 200ms;
+          &:hover {
+            background-color: rgba(0, 0, 0, 0.1);
+          }
+        `}
+      />
+    </Box>
+  )
+}
 
 export default () => {
   const globalData = useGlobalData()
-  return (
-    <Box width={[1, 256, 320]} px={4} pt={4}>
-      <Heading>Rokt33r's blog</Heading>
-      <Box mb="auto">
-        <Link>About me</Link>
-        <Box>Posts</Box>
 
+  return (
+    <NavContainer width={[1, 256]}>
+      <NavLinkList>
+        <NavLinkListItem href='/' fontSize='3'>
+          <Avartar />
+          Rokt33r's Lab
+        </NavLinkListItem>
+        <NavLinkListItem href='/about'>👨‍🚀 /about</NavLinkListItem>
+        <NavLinkListItem href='/posts'>✍️ /posts</NavLinkListItem>
         {globalData.categories.map(category => (
-          <Box key={category}>{category}</Box>
+          <NavLinkListItem key={category} href={`/posts?category=${category}`}>
+            ?category={category}
+          </NavLinkListItem>
         ))}
-      </Box>
-    </Box>
+      </NavLinkList>
+    </NavContainer>
   )
 }
