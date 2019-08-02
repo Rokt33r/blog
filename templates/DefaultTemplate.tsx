6a8406872e4cfe58e66e20861c345555eb1a903c
Junyoung Choi <fluke8259@gmail.com>
2019-08-02 23:53:29 +0900

    Add Copyright footer

--- templates/DefaultTemplate.tsx
+++ templates/DefaultTemplate.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Flex from '../atoms/Flex'
 import Container from '../atoms/Container'
 import Navigator from '../organisms/Navigator'
+import Text from '../atoms/Text'
 
 interface DefaultTemplateProps {
   children: React.ReactNode
@@ -12,6 +13,9 @@ export default ({ children }: DefaultTemplateProps) => (
     <Navigator />
     <Container mx='auto' px={4} pb={6} width={1}>
       {children}
+      <Text my={4} as='footer' color='#777' textAlign='center'>
+        © 2019 Junyoung Choi
+      </Text>
     </Container>
   </Flex>
 )
