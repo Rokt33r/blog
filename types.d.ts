821587f4bdc62d926c3310dc641b78765bb78e3c
Junyoung Choi <fluke8259@gmail.com>
2019-08-03 12:43:28 +0900

    Add temporary types

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,6 @@
 declare module 'remark-parse' {}
 declare module 'remark-frontmatter' {}
 declare module 'remark-react' {}
+declare module 'rehype-highlight' {}
+declare module 'remark-rehype' {}
+declare module 'rehype-react' {}
