e472da98da4940546e64147c23eed28bd6cde17b
Junyoung Choi <fluke8259@gmail.com>
2019-08-10 11:27:05 +0900

    No more apis

--- api/global.ts
+++
@@ -1,8 +0,0 @@
-import ky from 'ky-universal'
-import { baseURL } from '../lib/consts'
-
-export interface GlobalResponseBody {}
-
-export function fetchGlobalData(): Promise<GlobalResponseBody> {
-  return ky(baseURL + '/api/global').json()
-}
