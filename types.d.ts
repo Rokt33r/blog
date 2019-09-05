ddff24105262403d8d9292a06bb431181f2ef034
Junyoung Choi <fluke8259@gmail.com>
2019-09-05 23:29:49 +0900

    Clean up definition files

--- types.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'remark-parse' {}
-declare module 'remark-frontmatter' {}
-declare module 'remark-react' {}
-declare module 'rehype-highlight' {}
-declare module 'remark-rehype' {}
-declare module 'rehype-react' {}
