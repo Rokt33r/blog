88bd38b75ac1e175a465fbfba489ff4f41a454cb
Junyoung Choi <fluke8259@gmail.com>
2019-09-07 04:01:12 +0900

    Improve footer in default template

--- templates/DefaultTemplate.tsx
+++ templates/DefaultTemplate.tsx
@@ -18,13 +18,7 @@ export default ({ children }: DefaultTemplateProps) => (
     <Navigator />
     <Container mx='auto' px={4} pb={6} width={1}>
       {children}
-      <Box my={4} as='footer'>
-        <Text textAlign='center'>
-          <Link href='https://github.com/Rokt33r/blog' target='_blank'>
-            <Icon path={mdiGithubCircle} size='1em' />
-            Rokt33r/blog
-          </Link>
-        </Text>
+      <Box my={5} as='footer'>
         <Flex justifyContent='center'>
           <Text color='#777' textAlign='center' mr={2}>
             © 2019
@@ -33,6 +27,12 @@ export default ({ children }: DefaultTemplateProps) => (
             <Link mr={2}>Junyoung Choi</Link>
           </NextLink>
         </Flex>
+        <Text textAlign='center' color='#777'>
+          All source codes are available in{' '}
+          <Link href='https://github.com/Rokt33r/blog' target='_blank'>
+            <Icon path={mdiGithubCircle} size='1em' /> Rokt33r/blog
+          </Link>
+        </Text>
       </Box>
     </Container>
   </Flex>
