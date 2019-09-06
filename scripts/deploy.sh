055fc89ffdadc3ab5bffa7d47a0e7dcb89a28f17
Junyoung Choi <fluke8259@gmail.com>
2019-09-07 04:08:01 +0900

    Hard-code blog repository endpoint

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@ cd out
 git init
 git add -A
 git commit -a -m "Deployed at $(date)"
-git remote add origin $GITHUB_URL
+git remote add origin git@github.com:Rokt33r/rokt33r.github.io.git
 git push -f origin master
 rm -Rf .git
